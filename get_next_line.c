3c65de9938790f3356189197749cfb29eae71bb4
Zaid NAZAM <znazam@c4r7s8.wethinkcode.co.za>
2019-06-13 15:48:40 +0200

    edited

---
+++ get_next_line.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: znazam <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/10 11:01:26 by znazam            #+#    #+#             */
+/*   Updated: 2019/06/13 15:48:21 by znazam           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+void	checker(const int fd, char **check)
+{
+	char buff[BUF_SIZE + 1];
+	int *tmp;
+	int ret;
+
+}
+
+int		get_next_line(const int fd, char **line)
+{
+
+}
